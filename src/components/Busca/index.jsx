955181ef025b0147ca2124ab1b9d76d1d76c7fd1
natcurti <naticurti01@gmail.com>
2024-04-09 17:29:11 -0300

    Handle search input

--- src/components/Busca/index.jsx
+++ src/components/Busca/index.jsx
@@ -1,9 +1,26 @@
+import { mudarBusca, resetarBusca } from "src/store/reducers/busca";
 import styles from "./Busca.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Busca() {
+  const busca = useSelector((state) => state.busca);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(resetarBusca());
+  }, [location.pathname, dispatch]);
+
   return (
     <div className={styles.busca}>
-      <input className={styles.input} placeholder="O que você procura?" />
+      <input
+        className={styles.input}
+        placeholder="O que você procura?"
+        value={busca}
+        onChange={(e) => dispatch(mudarBusca(e.target.value))}
+      />
     </div>
   );
 }
