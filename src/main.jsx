f30e24522a19fbe20e37f5973ccd9c1196c61b9d
natcurti <naticurti01@gmail.com>
2024-04-22 12:52:32 -0300

    Create standalone toast with chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,18 @@ import Router from "./routes";
 import "./index.css";
 import store from "./store";
 import { Provider } from "react-redux";
+import { createStandaloneToast } from "@chakra-ui/react";
+
+const { ToastContainer, toast } = createStandaloneToast();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router />
+    <ToastContainer />
   </Provider>
 );
+
+toast({
+  description: "Unable to create user account.",
+  duration: 2000,
+});
