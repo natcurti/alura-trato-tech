028a95ae50c013178fad84ea52fd6379329194b7
natcurti <naticurti01@gmail.com>
2024-04-29 14:55:15 -0300

    Install redux saga and apply saga middleware to fetch categories

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,10 @@ import carrinhoSlice from "./reducers/carrinho";
 import buscaSlice from "./reducers/busca";
 import { categoriasListener } from "./middlewares/categorias";
 import { itensListener } from "./middlewares/itens";
+import createSagaMiddleware from "redux-saga";
+import { categoriasSaga } from "./sagas/categorias";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
@@ -15,9 +19,12 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().prepend(
-      categoriasListener.middleware,
-      itensListener.middleware
+      // categoriasListener.middleware,
+      itensListener.middleware,
+      sagaMiddleware
     ),
 });
+
+sagaMiddleware.run(categoriasSaga);
 
 export default store;
---
+++ src/store/sagas/categorias.js
@@ -0,0 +1,47 @@
+import { call, put, delay, takeLatest } from "redux-saga/effects";
+import {
+  adicionarTodasCategorias,
+  carregarCategorias,
+} from "../reducers/categorias";
+import { createStandaloneToast } from "@chakra-ui/react";
+import categoriasService from "src/services/categorias-service";
+
+const { toast } = createStandaloneToast();
+
+function* observarCategorias() {
+  yield console.log("observando.");
+  toast({
+    title: "Carregando",
+    description: "Carregando categorias",
+    duration: 500,
+    isClosable: true,
+    status: "loading",
+  });
+  try {
+    yield delay(1000);
+    const categorias = yield call(categoriasService.buscar);
+    console.log(categorias);
+    yield put(adicionarTodasCategorias(categorias));
+    toast({
+      title: "Sucesso!",
+      description: "Categorias carregadas com sucesso.",
+      duration: 1000,
+      isClosable: true,
+      status: "success",
+    });
+  } catch (error) {
+    toast({
+      title: "Erro",
+      description: "Erro ao buscar categorias.",
+      duration: 500,
+      isClosable: true,
+      status: "error",
+    });
+  }
+}
+
+export function* categoriasSaga() {
+  const tarefa = yield takeLatest(carregarCategorias, observarCategorias);
+  console.log(tarefa);
+  yield takeLatest(adicionarTodasCategorias, () => tarefa.cancel());
+}
